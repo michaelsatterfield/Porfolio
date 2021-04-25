445fc532df33b50a6fbd461c9475cd7275a3d1b8
Michael Satterfield <michael.f.satterfield@gmail.com>
2021-04-25 11:24:39 -0500

    adjusting

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -33,7 +33,7 @@ export default class blogPost extends Component {
         <SEO
           title={data.title}
           keywords={[
-            `Rohit Gupta`,
+            `Michael Satterfeild`,
             `Frontend Developer`,
             `Developer`,
             `${data.title}`
